5f922c16e39a9b98e0470b0bb815a6f0b24c1b46
d.shipilov <d.shipilov@vk.team>
2024-10-12 12:19:54 +0300

    add get_h3_cells_from_polygon()

--- modules/h3_utils.py
+++ modules/h3_utils.py
@@ -1,7 +1,14 @@
+import h3
 from modules.common_types import H3CellId, Coordinate
 
 
 def get_h3_cells_from_polygon(
     polygon: list[Coordinate], resolution: int
 ) -> list[H3CellId]:
-    return []
+    list_of_tuples = [(coord.lat, coord.lon) for coord in polygon]
+    shape = h3.LatLngPoly(list_of_tuples)
+
+    string_cells = h3.h3shape_to_cells(shape, resolution)
+    cells = [h3.str_to_int(cell) for cell in string_cells]
+
+    return cells
