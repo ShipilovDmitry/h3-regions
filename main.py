52f6dcb19665197c826fae3a8fed0cbecbc394db
d.shipilov <d.shipilov@vk.team>
2025-01-17 14:51:55 +0300

    h3 11 level

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import multiprocessing
 from modules.parse_utils import read_tsv, read_tsv_queue, get_name_from_geojson
 from modules.wbk_utils import get_h3_cells_from_wkb
 from modules.serializer import cell_ids_to_bytes
-from modules.sql_writer import SQLWriter, SQLRegion
+from modules.sql_writer import SQLWriter, SQLWriterCellsCount, SQLRegion, SQLRegionCellsCount
 
 
-PATH_TO_FILE: str = "/Users/d.shipilov/vkmaps/h3-regions/cut-town-city-village.jsonl"
+PATH_TO_FILE: str = "/Users/d.shipilov/vkmaps/h3-regions/town-city-village.jsonl"
 FILENAME = Path(PATH_TO_FILE).stem
 
-H3_RESOLUTION: int = 10
+H3_RESOLUTION: int = 11
 
 
 def sql_insert(rows: multiprocessing.Queue) -> None:
-    sql_writer = SQLWriter(FILENAME)
+    sql_writer = SQLWriterCellsCount(FILENAME)
     while True:
         row = rows.get()
         if row is None: # wait for Poison pill
@@ -38,6 +38,18 @@ def h3_long_operation(region_from_json: str) -> SQLRegion | None:
     return sql_region
 
 
+def h3_long_opearation_count_cells(region_from_json: str) -> SQLRegionCellsCount:
+    try:
+        cells = get_h3_cells_from_wkb(region_from_json.wkb, H3_RESOLUTION)
+    except Exception as e:
+        print(e)
+        return None
+
+    region_name = get_name_from_geojson(region_from_json.attributes_geojson)
+    sql_region = SQLRegionCellsCount(region_from_json.id, region_name, len(cells))
+    return sql_region
+
+
 def multiprocessing_main():
     # Create the queues
     lines_from_file = multiprocessing.Queue()
@@ -54,7 +66,7 @@ def multiprocessing_main():
     # Create a process pool and map the process_data function to the input queue
     with multiprocessing.Pool() as pool:
         for sql_row in pool.imap_unordered(
-            h3_long_operation, iter(lines_from_file.get, None)
+            h3_long_opearation_count_cells, iter(lines_from_file.get, None)
         ):
             sql_rows.put(sql_row)
 
@@ -96,7 +108,7 @@ def main():
     remove_db()
 
     # sync_main()
-    # multiprocessing_main()
+    multiprocessing_main()
 
 
 if __name__ == "__main__":
